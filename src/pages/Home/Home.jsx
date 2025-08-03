431942cdb1c59821e756759610a674d7afa6f09b
MohamadAli20 <mohamadali.calanda@cvsu.edu.ph>
2025-08-03 08:50:28 +0800

    Initial commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Home.module.css';
+
+const Home = () => {
+  return (
+    <section className={styles.home}>
+      <h1>Welcome to My Portfolio</h1>
+      <p>I’m a React Developer focused on building beautiful web experiences.</p>
+    </section>
+  );
+};
+
+export default Home;
